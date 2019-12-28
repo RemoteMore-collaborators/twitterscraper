9f83583226ec1954427e7366a318179123ea5c56
Copser <ppilipovic84@gmail.com>
2019-12-28 19:12:47 +0100

    preparing for heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :twitter_feed, TwitterFeedWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "peaceful-reaches-79794", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
