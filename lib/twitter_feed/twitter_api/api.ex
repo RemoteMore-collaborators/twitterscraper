c34b51d2fc0dbb6a996ab5ac705bcd450cd625bd
Copser <ppilipovic84@gmail.com>
2020-01-06 08:06:01 +0100

    create new callback for twitter status page

--- lib/twitter_feed/twitter_api/api.ex
+++ lib/twitter_feed/twitter_api/api.ex
@@ -1,4 +1,5 @@
 defmodule TwitterFeed.TwitterApi.Api do
   @callback get_home_page(handle :: String.t) :: String.t
+  @callback get_status_page(handle :: String.t, tweet_id :: Integert.t) :: String.t
   @callback get_tweets(handle :: String.t, last_tweet_retrieved :: Integer.t) :: String.t
 end
