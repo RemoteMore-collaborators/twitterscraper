a4a35ffd6533143b5eb7dd089cf27841f3deb663
Copser <ppilipovic84@gmail.com>
2020-01-16 14:20:10 +0100

    add more data from tweet.json

--- lib/spreadsheet.ex
+++ lib/spreadsheet.ex
@@ -38,25 +38,27 @@ defmodule SpreadSheet do
 
   def testing_scrape(handle) do
     scrape =
-      ExTwitter.search(handle, [count: 15])
+      ExTwitter.search(handle, [count: 5])
         |> Enum.map(fn(tweet) ->
-          String.split(tweet.text, "  ", trim: true)
+          [
+            tweet.favorite_count
+          ]
         end)
-        |> Enum.join("\n-----\n")
+        |> Enum.join("\n--------\n")
         |> IO.puts
 
     {:ok, scrape}
   end
 
   def save_to_spreadsheet(handle) do
-    {:ok, pid} = GSS.Spreadsheet.Supervisor.spreadsheet("1XvvLVKqmFEdipxB5uX8Sp0tIAzEevI_F9xeZ1iq1Y1s", list_name: "TwitterStatusScrape")
+    {:ok, pid} = GSS.Spreadsheet.Supervisor.spreadsheet("1XvvLVKqmFEdipxB5uX8Sp0tIAzEevI_F9xeZ1iq1Y1s", list_name: "CandyCrushSagaScrape")
 
     save_to_candy =
-      ExTwitter.search(handle, [count: 100])
+      ExTwitter.search(handle, [count: 10])
         |> Enum.with_index(1)
         |> Enum.each(fn {tweet, index} ->
           GSS.Spreadsheet.append_row(pid, index,
-            String.split(tweet.text, "  ", trim: true)
+            [tweet.user.screen_name, tweet.text, tweet.favorite_count]
           )
         end)
 
